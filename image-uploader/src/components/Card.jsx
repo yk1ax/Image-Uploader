413eb2bcc9014e9d53b22b7bd88e4340714eb99f
yogesh-ti <yogesh.kumar@trilogy.com>
2022-10-21 18:00:49 +0530

    Added UploadedCard

--- image-uploader/src/components/Card.jsx
+++ image-uploader/src/components/Card.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
-import Card from 'react-bootstrap/Card';
 import UploadCard from "./UploadCard";
 import UploadingCard from "./UploadingCard"
 import UploadedCard from "./UploadedCard";
 
 function CustomCard() {
 
-  const [uploading, setUploading] = useState(true);
-  const [uploaded, setUploaded] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [uploaded, setUploaded] = useState(true);
   return (
     uploading ?
       <UploadingCard />
--- image-uploader/src/components/UploadedCard.jsx
+++ image-uploader/src/components/UploadedCard.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import Button from 'react-bootstrap/Button';
+
 
 export default function UploadedCard() {
+    const imgSrc = "https://picsum.photos/400/300";
     return (
         <Card className="card p-3">
-        <Card.Body>
-            
-        </Card.Body>
+            <Card.Body>
+                <CheckCircleIcon className="done-icon" />
+                <h3>Uploaded Successfully!</h3>
+                <img className="uploaded-img" src={imgSrc} alt="" />
+                <div className="copy-link">
+                    <input type="text" style={{ border: "none transparent", outline: "none", minWidth: "20px", borderRadius: "8px" }} readOnly />
+                    <Button style={{marginLeft: "auto"}} >Copy link</Button>
+                </div>
+            </Card.Body>
         </Card>
     );
 }
